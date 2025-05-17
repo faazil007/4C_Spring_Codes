d828dcfb76cd292bceab69955e0c2b325a359fc8
Muhammad Fazil <malikfazil1110999@gmail.com>
2025-05-17 12:39:43 +0500

    program added

--- codes/task30.asm
+++ codes/task30.asm
@@ -2,77 +2,81 @@
 .STACK 100h
 
 .DATA
-; No data needed for this code
+; Koi static data nahi
 
 .CODE
-MAIN PROC
-    CALL INPUTDEC       ; Call the procedure to get integer input
-    CALL OUTPUTDEC
-    MOV AH, 4CH      ; Exit to DOS
-    INT 21H
-MAIN ENDP
 
-;-----------------------------------------
-INPUTDEC PROC
-    MOV BX, 0        ; BX will store the final numeric result
-    MOV CX, 10       ; Multiplier for place value (not needed for one-digit input here)
-
-    MOV AH, 1        ; DOS function to read character from keyboard
-    INT 21H          ; Input in AL
-
-    CMP AL, '0'      ; Check if input is >= '0'
-    JB INVALID_INPUT
-
-    CMP AL, '9'      ; Check if input is <= '9'
-    JA INVALID_INPUT
-
-    SUB AL, '0'      ; Convert ASCII digit to integer (e.g., '5' -> 5)
-    MOV AH, 0        ; Clear AH to form a full 16-bit value
-    MOV AX, AX       ; AX now has the integer value
-    MOV BX, AX       ; Store it in BX
-
-    JMP RETURN_RESULT
+InDec PROC
+    ; 1_Initialization
+    MOV BX, 0       ; For Intermediate Result
+    MOV CX, 10      ; For Multiplier for Decimal Number
     
+InDec1:   
+    ; 2_Input Char
+    MOV AH, 1       ; Input char will be in AL
+    INT 21h
     
-                     
-                                       
-
-INVALID_INPUT:
-    MOV BX, 0        ; Set result to 0 if invalid input
-
-RETURN_RESULT:
-    MOV AX, BX       ; Return result in AX
+    ; 3_Verify Whether it is Digit Char or Not
+    CMP AL, '0'
+    JB InDec2
+    CMP AL, '9'
+    JA InDec2
+    
+    ; 4_Convert Digit char into Digit
+    SUB AL, '0'     ; Or SUB AL, 30h or 48
+    
+    ; Multiply IR with Multiplier
+    MOV AH, 0       ; Make AH zero before starting
+    PUSH AX         ; Save digit in stack
+    
+    MOV AX, BX      ; Multiplicand (IR/BX) into AX
+    MUL CX          ; DX:AX = AX * CX
+    
+    ; 6_ Add the Digit into new IR
+    POP BX          ; Place the digit in BX from stack
+    ADD BX, AX      ; BX = BX + AX
+    
+    ; 7_Goto
+    JMP InDec1
+    
+InDec2:
+    MOV AX, BX      ; Return the converted value in AX
     RET
-INPUTDEC ENDP
+InDec ENDP
 
-
-
-
-OUTPUTDEC PROC                    ; This procedure displays the value stored in AX register as a decimal integer
-    MOV BX,10        ; To be used as divisor
-    MOV CX,0         ; To be used as digit counter
-                     
-                     ; Separate each of the digits in the integer value 
-OUTPUTDEC1:
-    MOV DX,0         ; Higher part is zero, max input value is of higher bytes                 
-    DIV BX           ; (DX:AX)/BX Quotient in AX and Remainder in DX
-                     
-    PUSH DX          ; Push the remainder on to the stack
-                     ; The separated
-                     
-    INC CX           ; Increase the digit count             ;Repeat the above procedure until all digits are separated
-    CMP AX,0         ; Check whether the quotient is zero
-    JNZ OUTPUTDEC1
+Outdec PROC
+    ; Display the value stored in AX as a decimal integer
     
-OUTPUTDEC2:
-    POP DX
-    ADD DL,'0'
-    MOV AH,2
-    INT 21H
+    MOV BX, 10      ; Divisor
+    MOV CX, 0       ; Digit counter
     
-    LOOP OUTPUTDEC2
+Outdec1:
+    MOV DX, 0       ; Higher part zero for DIV
+    DIV BX          ; Divide AX by BX, quotient in AX, remainder in DX
+    
+    PUSH DX         ; Push remainder (digit) on stack
+    INC CX          ; Increase digit count
+    
+    CMP AX, 0       ; Check if quotient zero
+    JNZ Outdec1     ; If not zero repeat
+    
+Outdec2:
+    POP DX          ; Pop digit from stack
+    ADD DL, '0'     ; Convert to ASCII
+    
+    MOV AH, 2       ; DOS function to print char
+    INT 21h
+    
+    LOOP Outdec2    ; Loop CX times
+    
     RET
-    OUTPUTDEC ENDP                         
+Outdec ENDP
 
-;-----------------------------------------
-END MAIN
+Main PROC
+    MOV AX, 64937
+    CALL Outdec
+    MOV AH, 4Ch     ; Terminate program
+    INT 21h
+Main ENDP
+
+END Main
