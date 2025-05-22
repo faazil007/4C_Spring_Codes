0dc6871e0b20c8d818b064006d5182b16f2c8c34
Muhammad Fazil <malikfazil1110999@gmail.com>
2025-05-22 16:27:36 +0500

    XX

--- Assignments/Assignment4/Q#NO2(BINARY).asm
+++ Assignments/Assignment4/Q#NO2(BINARY).asm
@@ -1,297 +1,382 @@
-.MODEL SMALL
+                      .MODEL SMALL
 .STACK 100H
+;-------------------------------------------------------------------------
 .DATA
-    MENU DB 10, 13, "***SELECT OPERATION***",
-             DB 10, 13, "1. ADDITION",
-             DB 10, 13, "2. SUBTRACTION",
-             DB 10, 13, "3. MULTIPLICATION",
-             DB 10, 13, "4. DIVISION(REMAINDER)",
-             DB 10, 13, "5. DIVISION(QUOTIENT)",
-             DB 10, 13, "6. EXIT$"
-    MSG1 DB 10, 13, "PLEASE INPUT A VALID DIGITAL CHARACTER...PRESS ANY KEY TO CONTINUE...$"
-    MSG2 DB 10, 13, "ENTER FIRST VALUE(BINARY):$"
-    MSG3 DB 10, 13, "ENTER SECOND VALUE(BINARY):$"
-    MSG4 DB 10, 13, "RESULT = $"
-    MSG5 DB 10, 13, "PRESS ANY KEY TO CONTINUE...$"
-    OP1 DW ?
-    OP2 DW ?
-    ADD_OPR DB "+$"
-    SUB_OPR DB "-$"
-    MUL_OPR DB "*$"
-    REM_OPR DB "%$"
-    QUO_OPR DB "/$"
-    EQUALS DB "=$"
-    TEMP DB ?
+    MENU DB 0DH,0AH,  " BINARY CALCULATOR ",
+         DB 0DH,0AH,  " -------MENU--------- ",
+         DB 0DH,0AH,   "PRESS 1 FOR ADDITION           ::",
+         DB 0DH,0AH,   "PRESS 2 FOR SUBTRACTION        ::",
+         DB 0DH,0AH,   "PRESS 3 FOR MULTIPLICATION     ::",
+         DB 0DH,0AH,   "PRESS 4 FOR DIVISION(REMAINDER)::",
+         DB 0DH,0AH,   "PRESS 5 FOR DIVISION(QUOTIENT) ::",
+         DB 0DH,0AH,   "PRESS 6 TO EXIT$ " 
+         
+         
+     
+     MSG1 DB 10,13, "ENTER FIRST BINARY ::,$"
+     MSG2 DB 10,13, "ENTER SECOND BINARY::,$"
+     MSG3 DB 10,13, "ERROR! ENTER A VALID BINARY VALUE::,$"
+     MSG4 DB 10,13, "RESULT OF CALCULATION::,$"
+     MSG5 DB 10,13, "PRESS ANY KEY TO PROCEED!!!,$"
+     NEWLINE DB 0Dh, 0Ah, '$'
+     PLUS_OPERATOR      DB "+$"
+     MINUS_OPERATOR     DB "-$"
+     MULTIPLY_OPERATOR  DB "*$"
+     REMAINDER_OPERATOR DB "%$"
+     QUOTIENT_OPERATOR  DB "/$"
+     EQUALS_OPERATOR    DB "=$"
+     TEMPORARY DB ?
+     OPERAND1 DW ?
+     OPERAND2 DW ?
+;--------------------------------------------------------------------------
 .CODE
     MAIN PROC
-        MOV AX, @DATA
-        MOV DS, AX
+        MOV AX,@DATA
+        MOV DS,AX
+        
+        
+        
         START:
+            MOV DX, OFFSET MENU
+            CALL STRING_DISPLAY
             
-            LEA DX, MSG2
-            CALL STR_DIS
-            CALL BIN_INP
-            MOV OP1, AX
-                       
-            LEA DX, MSG3
-            CALL STR_DIS
-            CALL BIN_INP
-            MOV OP2, AX
+            MOV DX, OFFSET NEWLINE
+            CALL STRING_DISPLAY
             
-            PROCESS:
-                LEA DX, MENU
-                CALL STR_DIS
-                
-                CALL NEW_LINE
-                CALL CHAR_INP
-                MOV BL, TEMP
-                
-                CMP BL, '1'
-                JZ ADDITION
-                
-                CMP BL, '2'
-                JZ SUBTRACTION
-                
-                CMP BL, '3'
-                JZ MULTIPLICATION
-                
-                CMP BL, '4'
-                JZ DIV_REM
-                
-                CMP BL, '5'
-                JZ DIV_QUO
-                
-                CMP BL, '6'
-                JZ QUIT
-                
-                JNZ ERROR
-                
-                ADDITION:               
-                    CALL NEW_LINE
-                    MOV AX, OP1
-                    CALL BIN_OUT
-                    LEA DX, ADD_OPR
-                    CALL STR_DIS
-                    MOV AX, OP2
-                    CALL BIN_OUT
-                    LEA DX, EQUALS
-                    CALL STR_DIS
-                    
-                    MOV BX, OP1
-                    ADD BX, OP2
-                    
-                    MOV AX, BX
-                    CALL BIN_OUT
-                    
-                    LEA DX, MSG5
-                    CALL STR_DIS
-                    CALL CHAR_INP
-                    
-                    JMP START
-     
-                SUBTRACTION:
-                    CALL NEW_LINE
-                    MOV AX, OP1
-                    CALL BIN_OUT
-                    LEA DX, SUB_OPR
-                    CALL STR_DIS
-                    MOV AX, OP2
-                    CALL BIN_OUT
-                    LEA DX, EQUALS
-                    CALL STR_DIS
-                    
-                    MOV BX, OP1
-                    SUB BX, OP2
-                    
-                    MOV AX, BX
-                    CALL BIN_OUT
-                    
-                    LEA DX, MSG5
-                    CALL STR_DIS
-                    CALL CHAR_INP
-                    
-                    JMP START
-                
-                MULTIPLICATION:
-                    CALL NEW_LINE
-                    MOV AX, OP1
-                    CALL BIN_OUT
-                    LEA DX, MUL_OPR
-                    CALL STR_DIS
-                    MOV AX, OP2
-                    CALL BIN_OUT
-                    LEA DX, EQUALS
-                    CALL STR_DIS
-                    
-                    MOV AX, OP1
-                    MUL OP2
-                    
-                    CALL BIN_OUT
-                    
-                    LEA DX, MSG5
-                    CALL STR_DIS
-                    CALL CHAR_INP
-                    
-                    JMP START
-                
-                DIV_REM:
-                    CALL NEW_LINE
-                    MOV AX, OP1
-                    CALL BIN_OUT
-                    LEA DX, REM_OPR
-                    CALL STR_DIS
-                    MOV AX, OP2
-                    CALL BIN_OUT
-                    LEA DX, EQUALS
-                    CALL STR_DIS
-                    
-                    MOV AX, 0
-                    MOV DX, 0
-                    MOV AX, OP1
-                    DIV OP2
-                    MOV AX, 0
-                    MOV AX, DX
-                    
-                    CALL BIN_OUT
-                    
-                    LEA DX, MSG5
-                    CALL STR_DIS
-                    CALL CHAR_INP
-                    
-                    JMP START
-                
-                DIV_QUO:
-                    CALL NEW_LINE
-                    MOV AX, OP1
-                    CALL BIN_OUT
-                    LEA DX, QUO_OPR
-                    CALL STR_DIS
-                    MOV AX, OP2
-                    CALL BIN_OUT
-                    LEA DX, EQUALS
-                    CALL STR_DIS
-                    
-                    MOV AX, 0
-                    MOV DX, 0
-                    MOV AX, OP1
-                    DIV OP2
-                    
-                    CALL BIN_OUT
-                    
-                    LEA DX, MSG5
-                    CALL STR_DIS
-                    CALL CHAR_INP
-                    
-                    JMP START
-                
-                ERROR:
-                    LEA DX, MSG1
-                    CALL STR_DIS
-                    CALL CHAR_INP
-                    JMP PROCESS
-                
-                JMP QUIT
+            START2:
+                CALL CHARACTER_INPUT
+                MOV BL,TEMPORARY
             
-        QUIT:
-            MOV AH, 4CH
-            INT 21H
+                CMP BL,'1'
+                JE ADDITION
+            
+                CMP BL,'2'
+                JE SUBTRACTION
+            
+                CMP BL,'3'
+                JE MULTIPLICATION
+            
+                CMP BL,'4'
+                JE CHECKING_REMAINDER
+            
+                CMP BL,'5'
+                JE CHECKING_QUOTIENT
+            
+                CMP BL,'6'
+                JE QUIT
+            
+                JNE ERROR
+           ;----------------------------------------------------------------- 
+            ADDITION:
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                
+                MOV DX, OFFSET MSG1
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND1, AX
+            
+                MOV DX, OFFSET MSG2
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND2, AX 
+                
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                                
+                MOV AX,OPERAND1
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET PLUS_OPERATOR
+                CALL STRING_DISPLAY 
+                
+                MOV AX,OPERAND2
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET EQUALS_OPERATOR
+                CALL STRING_DISPLAY
+                
+                MOV BX, OPERAND1
+                ADD BX,OPERAND2
+                
+                MOV AX,BX
+                CALL BINARY_OUTPUT
+                
+                MOV DX, OFFSET MSG5
+                CALL STRING_DISPLAY
+                CALL CHARACTER_INPUT
+                JMP START
+          ;--------------------------------------------------------------------      
+            SUBTRACTION:
+                
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                
+                MOV DX, OFFSET MSG1
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND1, AX
+            
+                MOV DX, OFFSET MSG2
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND2, AX                
+                
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV AX,OPERAND1
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET MINUS_OPERATOR
+                CALL STRING_DISPLAY 
+                
+                MOV AX,OPERAND2
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET EQUALS_OPERATOR
+                CALL STRING_DISPLAY
+                
+                MOV BX, OPERAND1
+                SUB BX,OPERAND2
+                
+                MOV AX,BX
+                CALL BINARY_OUTPUT
+                
+                MOV DX, OFFSET MSG5
+                CALL STRING_DISPLAY
+                CALL CHARACTER_INPUT
+                JMP START
+         ;-----------------------------------------------------------------------
+            MULTIPLICATION:
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV DX, OFFSET MSG1
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND1, AX
+            
+                MOV DX, OFFSET MSG2
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND2, AX                
+             
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV AX,OPERAND1
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET MULTIPLY_OPERATOR
+                CALL STRING_DISPLAY 
+                
+                MOV AX,OPERAND2
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET EQUALS_OPERATOR
+                CALL STRING_DISPLAY
+                
+                MOV AX, OPERAND1
+                MUL OPERAND2
+                CALL BINARY_OUTPUT
+                
+                MOV DX, OFFSET MSG5
+                CALL STRING_DISPLAY
+                CALL CHARACTER_INPUT
+                JMP START
+         ;-------------------------------------------------------------------   
+            CHECKING_REMAINDER:
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV DX, OFFSET MSG1
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND1, AX
+            
+                MOV DX, OFFSET MSG2
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND2, AX                
+                    
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV AX,OPERAND1
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET REMAINDER_OPERATOR
+                CALL STRING_DISPLAY 
+                
+                MOV AX,OPERAND2
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET EQUALS_OPERATOR
+                CALL STRING_DISPLAY
+                
+                MOV AX, 0
+                MOV DX, 0
+                MOV AX, OPERAND1
+                DIV OPERAND2
+                MOV AX, 0
+                MOV AX, DX 
+                
+                CALL BINARY_OUTPUT
+                
+                MOV DX, OFFSET MSG5
+                CALL STRING_DISPLAY
+                CALL CHARACTER_INPUT
+                JMP START
+          ;-------------------------------------------------------------------      
+             CHECKING_QUOTIENT:
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV DX, OFFSET MSG1
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND1, AX
+            
+                MOV DX, OFFSET MSG2
+                CALL STRING_DISPLAY
+                CALL BINARY_INPUT
+                MOV OPERAND2, AX                
+                    
+                MOV DX, OFFSET NEWLINE
+                CALL STRING_DISPLAY
+                
+                MOV AX,OPERAND1
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET QUOTIENT_OPERATOR
+                CALL STRING_DISPLAY 
+                
+                MOV AX,OPERAND2
+                CALL BINARY_OUTPUT
+                MOV DX, OFFSET EQUALS_OPERATOR
+                CALL STRING_DISPLAY   
+                
+            
+                MOV AX, 0
+                MOV DX, 0
+                MOV AX, OPERAND1
+                DIV OPERAND2
+                    
+                CALL BINARY_OUTPUT
+                    
+                MOV DX, OFFSET MSG5
+                CALL STRING_DISPLAY
+                CALL CHARACTER_INPUT
+                JMP START
+                
+          ;--------------------------------------------------------------------
+             ERROR:
+                MOV DX, OFFSET MSG3
+                CALL STRING_DISPLAY
+                MOV DX, OFFSET MSG5
+                CALL STRING_DISPLAY
+                CALL CHARACTER_INPUT
+                JMP START2
+                
+             JMP QUIT                
+             
+             QUIT:
+                MOV AH,4CH
+                INT 21H    
+                   
     MAIN ENDP
-    
-    CHAR_INP PROC
-        
-        MOV AH, 1
+
+;--------------------------------    
+; 1st Proc    
+;--------------------------------    
+    CHARACTER_INPUT PROC
+        MOV AH,1
         INT 21H
-        MOV TEMP, AL
+        MOV TEMPORARY,AL
         
         RET
-    CHAR_INP ENDP
-    
-    CHAR_OUT PROC
-        MOV AH, 2
-        INT 21H
-        
-        RET
-    CHAR_OUT ENDP
-    
-    BIN_INP PROC
-        MOV BX, 0
-        MOV CX, 2
-        
-        AGAIN1:
-            CALL CHAR_INP
-            MOV AL, TEMP
-            
-            CMP AL, '0'
-            JB DONE_INP
-            
-            CMP AL, '1'
-            JA DONE_INP
-            
-            SUB AL, '0'
-            
-            MOV AH, 0
-            
-            PUSH AX
-            
-            MOV AX, BX
-            
-            MUL CX
-            
-            POP BX
-            
-            ADD BX, AX
-            
-            JMP AGAIN1
-            
-        DONE_INP:
-            MOV AX, BX
-            
-        RET
-    BIN_INP ENDP
-    
-    BIN_OUT PROC
-        MOV BX, 2
-        MOV CX, 0
-        AGAIN2:        
-            MOV DX, 0
-            
-            DIV BX
-            
-            PUSH DX
-            
-            INC CX
-            
-            CMP AX, 0
-        
-            JNZ AGAIN2
-            
-        DISPLAY:
-            POP DX
-            
-            ADD DX, '0'
-            
-            CALL CHAR_OUT
-            
-            LOOP DISPLAY 
-        
-        RET
-    BIN_OUT ENDP
-    
-    NEW_LINE PROC
-        MOV AH, 2
-        MOV DL, 0DH
-        INT 21H
-        
+      
+    CHARACTER_INPUT ENDP
+;--------------------------------    
+
+;--------------------------------
+; 2nd Proc
+;--------------------------------    
+    CHARACTER_OUTPUT PROC
         MOV AH,2
-        MOV DL, 0AH
         INT 21H
         
         RET
-    NEW_LINE ENDP
+        
+    CHARACTER_OUTPUT ENDP    
+
+;--------------------------------
+; 3rd Proc
+;--------------------------------
+    STRING_DISPLAY PROC
+        MOV AH,9
+        INT 21H
+        
+        RET
+    STRING_DISPLAY ENDP
+       
+;--------------------------------
+; 4th Proc
+;--------------------------------
+    BINARY_INPUT PROC
+        MOV BX,0
+        MOV CX,2
+        
+        INPUT_BIN1:
+            CALL CHARACTER_INPUT
+            MOV AL,TEMPORARY
+            
+            CMP AL,'0'
+            JB INPUT_BIN2
+            
+            CMP AL,'1'
+            JA INPUT_BIN2
+            
+            SUB AL,'0'
+            MOV AH,0
+            PUSH AX
+            MOV AX,BX
+            MUL CX
+            POP BX
+            ADD BX,AX
+            JMP INPUT_BIN1
+            
+        
+        
+        INPUT_BIN2:
+            MOV AX,BX
+            
+        
+        RET
+        
+    BINARY_INPUT ENDP            
     
-    STR_DIS PROC
-        MOV AH, 9
-        INT 21H
+;--------------------------------
+; 5th Proc
+;--------------------------------
+    BINARY_OUTPUT PROC
+        MOV BX,2
+        MOV CX,0   
+        
+        OUTPUT_BIN1:
+            MOV DX,0
+            DIV BX
+            PUSH DX
+            INC CX
+            CMP AX,0
+            JNZ OUTPUT_BIN1
+        
+            
+        DISPLAY_DIGITS:
+            POP DX
+            ADD DX,'0'
+            CALL CHARACTER_OUTPUT
+            LOOP DISPLAY_DIGITS
         
         RET
-    STR_DIS ENDP
+        
+     BINARY_OUTPUT ENDP
+    
+;------------------------------------------------------------------------    
+            
+        
+        
